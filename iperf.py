d32d8190f2145466731ddb4994fa822ee7cc81b9
HuyLe <anhhuy@live.com>
2016-10-20 16:50:01 +0700

    add try expect

--- iperf.py
+++ iperf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-'''
+"""
 # *****************************************
 # Copyright  2016
 # AMCC, Inc. All Rights Reserved
@@ -8,19 +8,41 @@
 # Author: HuyLe,email: hule@apm.com
 ###########################################
 The script testing performance ethernet
-'''
-import sys, getopt, os
-import time
-#define time running iperf
+"""
+
+import sys, getopt, os, errno
+import time, py_compile
+
+if len(sys.argv) != 3:
+    print """\
+    ------------------------------------------------------------
+    Usage: ./iperf.py <iperf|iperf3> <serverip>
+    iperf or iperf3        The tools the testing performance networking
+    serverip               Server IP-Address
+    Notes: No.times running: [t=30]
+    ------------------------------------------------------------
+    """
+    sys.exit(1)
+#Define time running iperf
 time_t=30;
 command=sys.argv[1];
 server=sys.argv[2];
-
 #print ('CMD ' + str(command) + ' -c' + float(server) + '-t ' + (time_t));
-print ('CMD: %s -c %s -t %d -P ' % (command, server, time_t));
+#print ('CMD: %s -c %s -t %d -P ' % (command, server, time_t));
 #os.system('%s -c %s -t %d' % (command, server, time));
 threads=['1', '4', '8'];
-for i in range(len(threads)):
-    print ('No.threads: %s' %threads[i]);
-    os.system('%s -c %s -t %d -P %s' % (command, server, time_t, threads[i]));
-    time.sleep(10);
+try:
+    for i in range(len(threads)):
+        print ('------------------------------------------------------------');
+        print ('No.threads: %s' %threads[i]);
+        print ('CMD: %s -c %s -t %d -P %s' % (command, server, time_t, threads[i]));
+        os.system('%s -c %s -t %d -P %s' % (command, server, time_t, threads[i]));
+        time.sleep(1);
+except IOError as err:
+    print err.errno 
+    print err.strerror
+    print 'Does not see the server run %s -s' %command
+else:
+    print 'Run perfect!'
+finally:
+    print 'DONE.'
